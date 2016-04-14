68f502867bc75849c9e328796209752f06f13c63
zcx920206 <344651410@qq.com>
2016-04-14 14:42:38 +0800

    test-modifed

--- src/test/java/com/thoughtworks/shoppingweb/web/ProductControllerTest.java
+++ src/test/java/com/thoughtworks/shoppingweb/web/ProductControllerTest.java
@@ -10,10 +10,14 @@ import com.thoughtworks.shoppingweb.service.page.QueryFilter;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.*;
+import org.springframework.http.HttpMethod;
 import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.ui.ExtendedModelMap;
 import org.springframework.ui.Model;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 
 import java.util.List;
@@ -30,11 +34,16 @@ public class ProductControllerTest {
     @Mock
     ProductService productService;
     @Mock
+    HttpServletRequest request;
+    @Mock
     HistoryService historyService;
+    @Mock
+    private HttpSession session;
 
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
+        Mockito.when(request.getSession(true)).thenReturn(session);
         productController.productService=productService;
         productController.historyService=historyService;
 
@@ -82,10 +91,38 @@ public class ProductControllerTest {
         Mockito.when(productService.getProduct("123456")).thenReturn(productdetail);
         Mockito.when(historyService.insertHistory(user,history)).thenReturn(true);
         Model returnModel = new ExtendedModelMap();
-        MockHttpServletRequest request = new MockHttpServletRequest();
         productController.getProduct("123456",user.getUserName(),returnModel,request);
         Product product=(Product) returnModel.asMap().get("product");
         assertEquals(productdetail.getProductId(),product.getProductId());
 
+    }
+
+    @Test
+    public void createHistory() throws Exception {
+        List<History> historys=new ArrayList<History>();
+        History history=new History();
+        User user=new User();
+        user.setUserName("1");
+        historys.add(history);
+        Mockito.when(historyService.insertHistory(user,history)).thenReturn(true);
+        Mockito.when(historyService.getHistoryByUser(user.getUserName())).thenReturn(historys);
+        Model returnModel = new ExtendedModelMap();
+        productController.createHistory(user,"1",returnModel);
+        List<History> acturHistorys=(List<History>)returnModel.asMap().get("history");
+        assertEquals(historys,acturHistorys);
+    }
+
+    @Test
+    public void createUserWhenUserNameIsNotNull() throws Exception {
+        User user=new User();
+        user.setUserName("1");
+       User user1= productController.createUser(user.getUserName(),request);
+      assertEquals(user.getUserName(),user1.getUserName());
+    }
+    @Test
+    public void createUserWhenUserNameIsNull() throws Exception {
+        User user=new User();
+        User user1= productController.createUser(user.getUserName(),request);
+       assertNotNull(user1.getUserName());
     }
 }
